eba87f926c5514fc912fa16a482bfd664f786f3e
mattratto <matt.ratto@utoronto.ca>
2019-11-14 09:51:47 -0500

    added BLE to test code

--- inf351_testcode.ino
+++ inf351_testcode.ino
@@ -7,6 +7,25 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+
+//config for bluetooth
+#define BLUEFRUIT_SPI_CS               8
+#define BLUEFRUIT_SPI_IRQ              7
+#define BLUEFRUIT_SPI_RST              4    // Optional but recommended, set to -1 if unused
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
 #include <Adafruit_FeatherOLED.h>
 Adafruit_FeatherOLED oled = Adafruit_FeatherOLED();
 
@@ -20,6 +39,7 @@ int state = 0;
 #define BUTTON_B  6
 #define BUTTON_C  5
 bool lastButtonStateA; 
+bool lastButtonStateB; 
 bool buttonStateA; 
 bool buttonStateB; 
 bool buttonStateC; 
@@ -35,6 +55,57 @@ void setup()
 {
   Serial.begin(115200);
 
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    Serial.println(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      Serial.println("Couldn't factory reset");
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set module to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+
 oled.init();
 
 pinMode(BUTTON_A, INPUT_PULLUP); 
@@ -46,6 +117,14 @@ pinMode(BUTTON_C, INPUT_PULLUP);
 
 void loop()
 {
+
+ // Echo received data
+  while ( ble.available() )
+  {
+    int c = ble.read();
+    Serial.print((char)c);
+  }
+
 
 switch (state) {
   case 0:
@@ -68,11 +147,31 @@ switch (state) {
     while (digitalRead(BUTTON_A) == HIGH) {delay(10);}
     while (digitalRead(BUTTON_A) == LOW) {delay(10);}
     }
-    state =0; 
+    state =2; 
     break;
   case 2:
-    //not in use 
-    delay(1000); 
+    // Check for user input
+  char n, inputs[BUFSIZE+1];
+
+buttonStateA = digitalRead(BUTTON_A);
+    if (buttonStateA != lastButtonStateA) {
+      if (buttonStateA == LOW) { 
+      ble.print(tasks1[userTask1[0]]);  
+      }
+      }
+    lastButtonStateB = buttonStateB; 
+    
+  if (Serial.available())
+  {
+    n = Serial.readBytes(inputs, BUFSIZE);
+    inputs[n] = 0;
+    // Send characters to Bluefruit
+    Serial.print("Sending: ");
+    Serial.println(inputs);
+
+    // Send input data to host via Bluefruit
+    ble.print(inputs);
+  }
     break;
 }
 }
